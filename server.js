c1069ff38ba5a37449b53c6962dc255c11e62a32
Philip Maes <maes@leprchaun.io>
2015-02-06 09:10:35 +0000

    Migrate to published moneypot package for Node.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var config  = require('./config.json'),
     utility = require('./utility.js');
 
-var express   = require('express'),
+var Promise   = require('bluebird'),
+    express   = require('express'),
     exphbs    = require('express-handlebars'),
     fs        = require('fs'),
     http      = require('http'),
@@ -19,116 +20,103 @@ var logger = new winston.Logger({
 });
 
 var models = require('./models.js')(logger);
-var mp = new moneypot.Session(config.moneypot);
-function onMpConnect(error) {
-  if (error) {
-    logger.error('Unable to connect to MoneyPot: ' + error.message);
-    process.exit(1);
-  }
+
+new moneypot.Session().then(function(results) {
+  var session = results[0];
+
   logger.info('Successfully connected to MoneyPot.');
   models.ChatMessage.create({
     type: 'connect',
     timestamp: Date.now()
   });
-}
-if (config.moneypot.cookie) {
-  mp.connect(onMpConnect);
-} else {
-  mp.getCookie(function(error, cookie) {
-    if (error) {
-      logger.error('Unable to acquire new session cookie: ' + error.message);
-      process.exit(1);
-    }
-    logger.info('Acquired new session cookie.');
-    console.log(cookie);
-    mp.connect(onMpConnect);
-  });
-}
-mp.socket.on('disconnect', function() {
-  logger.info('Disconnected from MoneyPot.');
-  models.ChatMessage.create({
-    type: 'disconnect',
-    timestamp: Date.now()
-  });
-});
-mp.socket.on('game_crash', function(data) {
-  var id = mp.game.id;
-  models.Game.find({
-    where: { verified: 'multiplier' },
-    order: 'id DESC'
-  }).then(function(game) {
-    var rounds, expectedHash;
-    if (game !== null) {
-      rounds = id - game.id;
-      expectedHash = game.hash;
-    } else {
-      rounds = id - config.verification.offset + 1;
-      expectedHash = config.verification.terminatingHash;
-    }
 
-    var terminatingHash    = utility.terminatingHash(data.hash, rounds),
-        expectedMultiplier = utility.multiplierFromHash(data.hash, config.verification.clientSeed),
-        verified;
-    if (terminatingHash !== expectedHash) {
-      logger.warn('Hash mismatch in game ' + id + ' with received hash ' + data.hash + '.');
-      verified = 'none';
-    } else if (data.game_crash !== expectedMultiplier) {
-      logger.warn('Multiplier mismatch in game ' + id + ' with verified hash ' + data.hash + ' and multiplier '
-      + data.game_crash/100 + 'x (expected ' + expectedMultiplier/100 + 'x).');
-      verified = 'hash';
-    } else {
-      logger.verbose('Successfully verified game ' + id + '.');
-      verified = 'multiplier';
-    }
+  session.socket.on('disconnect', function() {
+    logger.info('Disconnected from MoneyPot.');
+    models.ChatMessage.create({
+      type: 'disconnect',
+      timestamp: Date.now()
+    });
+  });
 
-    models.Game.create({
-      id: id,
-      multiplier: data.game_crash,
-      hash: data.hash,
-      verified: verified
+  session.socket.on('game_crash', function(data) {
+    var id = session.game.id;
+    models.Game.find({
+      where: { verified: 'multiplier' },
+      order: 'id DESC'
     }).then(function(game) {
-      sockets.emit('game_verification', {
-        id: game.id,
-        multiplier: game.multiplier,
-        hash: game.hash,
-        verified: game.verified
+      var rounds, expectedHash;
+      if (game !== null) {
+        rounds = id - game.id;
+        expectedHash = game.hash;
+      } else {
+        rounds = id - config.verification.offset + 1;
+        expectedHash = config.verification.terminatingHash;
+      }
+
+      var terminatingHash    = utility.terminatingHash(data.hash, rounds),
+          expectedMultiplier = utility.multiplierFromHash(data.hash, config.verification.clientSeed),
+          verified;
+      if (terminatingHash !== expectedHash) {
+        logger.warn('Hash mismatch in game ' + id + ' with received hash ' + data.hash + '.');
+        verified = 'none';
+      } else if (data.game_crash !== expectedMultiplier) {
+        logger.warn('Multiplier mismatch in game ' + id + ' with verified hash ' + data.hash + ' and multiplier '
+        + data.game_crash/100 + 'x (expected ' + expectedMultiplier/100 + 'x).');
+        verified = 'hash';
+      } else {
+        logger.verbose('Successfully verified game ' + id + '.');
+        verified = 'multiplier';
+      }
+
+      models.Game.create({
+        id: id,
+        multiplier: data.game_crash,
+        hash: data.hash,
+        verified: verified
+      }).then(function(game) {
+        sockets.emit('game_verification', {
+          id: game.id,
+          multiplier: game.multiplier,
+          hash: game.hash,
+          verified: game.verified
+        });
       });
     });
   });
-});
-mp.socket.on('msg', function(message) {
-  switch (message.type) {
-    case 'say':
-      break;
-    case 'mute':
-      var duration = message.timespec.substring(0, message.timespec.length - 1),
-          unit     = message.timespec.substring(message.timespec.length - 1);
+  session.socket.on('msg', function(message) {
+    switch (message.type) {
+      case 'say':
+        break;
+      case 'mute':
+        var duration = message.timespec.substring(0, message.timespec.length - 1),
+            unit     = message.timespec.substring(message.timespec.length - 1);
 
-      switch (unit) {
-        case 'd': duration *= 24;
-        case 'h': duration *= 60;
-        case 'm': duration *= 60;
-        case 's': duration *= 1000;
-      }
+        switch (unit) {
+          case 'd': duration *= 24;
+          case 'h': duration *= 60;
+          case 'm': duration *= 60;
+          case 's': duration *= 1000;
+        }
 
-      message.type = message.shadow ? 'shadowmute' : 'mute';
-      var target = message.username;
-      message.username = message.moderator;
-      break;
-    default:
-      logger.warn('Unknown chat message type: ' + message.type);
-      return;
-  }
-  models.ChatMessage.create({
-    author: message.username,
-    authorRole: message.role,
-    type: message.type,
-    timestamp: new Date(message.time),
-    content: message.message,
-    target: target,
-    duration: duration
-  }).then(function(message) {
-    sockets.emit('chat_message', message);
+        message.type = message.shadow ? 'shadowmute' : 'mute';
+        var target = message.username;
+        message.username = message.moderator;
+        break;
+      default:
+        logger.warn('Unknown chat message type: ' + message.type);
+        return;
+    }
+    models.ChatMessage.create({
+      author: message.username,
+      authorRole: message.role,
+      type: message.type,
+      timestamp: new Date(message.time),
+      content: message.message,
+      target: target,
+      duration: duration
+    }).then(function(message) {
+      sockets.emit('chat_message', message);
+    });
   });
 });
 
